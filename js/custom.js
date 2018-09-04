f199a8ccd7c29650ec91811c659143427919681f
Juan Ignacio Verni <JuanVerni@JIV-TB.local>
2018-09-04 14:05:22 -0600

    wow.js added to services section

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// SERVICES SECTION
+
+// ACTIVATING WOW.JS PLUGIN
+
+//below we have the short form of $(document).ready(function (){});
+$(function() {
+	//animate on scroll
+	new WOW().init();
+});
