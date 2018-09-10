94003265f9dae0258d5d2bbf22d0b1183ae06376
Juan Ignacio Verni <JuanVerni@JIV-TB.local>
2018-09-10 17:01:25 -0600

    Magnific Popup plugin added to work section

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,21 @@
 // SERVICES SECTION
 
 // ACTIVATING WOW.JS PLUGIN
-
 //below we have the short form of $(document).ready(function (){});
 $(function() {
 	//animate on scroll
 	new WOW().init();
+});
+
+// WORK SECTION
+
+//Activating magnific-popup pluggin
+$(function() {
+  $("#work").magnificPopup({
+  	delegate: 'a', // child items selector, by clicking on it popup will open
+  	type: 'image',
+  	gallery: {
+  		enabled: true
+  	}
+  });
 });
