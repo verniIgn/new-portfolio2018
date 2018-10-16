c6cc36d3eec027a08ea23caffe76fd6cad9a062d
Juan Ignacio Verni <JuanVerni@jiv-tb.lan>
2018-10-16 11:19:47 -0600

    menu closes when item is clicked

--- js/custom.js
+++ js/custom.js
@@ -91,6 +91,48 @@ $(function() {
     });
 });
 
+// Close mobile menu on click
+$(function() {
+    $(".navbar-collapse ul li a").on("click touch", function() {
+        $(".navbar-toggle").click();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
