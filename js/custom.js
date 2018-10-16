983588b6cc17c5be7315b5166f7d2c509942fba5
Juan Ignacio Verni <JuanVerni@JIV-TB.local>
2018-10-15 18:21:10 -0600

    back to top button fades in out

--- js/custom.js
+++ js/custom.js
@@ -1,121 +1,95 @@
 // SERVICES SECTION
 
-			// ACTIVATING WOW.JS PLUGIN
+// ACTIVATING WOW.JS PLUGIN
 //below we have the short form of $(document).ready(function (){});
 $(function() {
-	//animate on scroll
-	new WOW().init();
+    //animate on scroll
+    new WOW().init();
 });
 
-			// WORK SECTION
+// WORK SECTION
 
 //Activating magnific-popup pluggin
 $(function() {
-  $("#work").magnificPopup({
-  	delegate: 'a', // child items selector, by clicking on it popup will open
-  	type: 'image',
-  	gallery: {
-  		enabled: true
-  	}
-  });
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
 });
 
-			// TEAM SECTION
+// TEAM SECTION
 $(function() {
-	$("#team-members").owlCarousel({
-		items: 3,
-		autoplay: true,
-		smartSpeed: 700,
-		loop: true,
-		autoplayHoverPause: true
-	});
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
 });
 
-			// TESTIMONIALS SECTION
+// TESTIMONIALS SECTION
 $(function() {
-	$("#customers-testimonials").owlCarousel({
-		items: 1,
-		autoplay: true,
-		smartSpeed: 700,
-		loop: true,
-		autoplayHoverPause: true
-	});
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
 });
 
-					// STATS COUNTER
+// STATS COUNTER
 $(function() {
-	$('.counter').counterUp({
-    	delay: 10,
+    $('.counter').counterUp({
+        delay: 10,
         time: 2000
     });
 });
 
 
-					// CLIENTS SECTION
+// CLIENTS SECTION
 $(function() {
-	$("#clients-list").owlCarousel({
-		items: 6,
-		autoplay: true,
-		smartSpeed: 700,
-		loop: true,
-		autoplayHoverPause: true
-	});
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
 });
 
-						// NAVBAR
+// NAVBAR
 // this will hide or show navbar according to where we are in the site
 $(function() {
-	$(window).scroll(function() {
-		if($(this).scrollTop() < 50 ) {
-			// hide background color of navbar
-			$("nav").removeClass("top-nav");
-		} else {
-			// show background color of navbar
-			$("nav").addClass("top-nav");
-		}
-	});
+    $(window).scroll(function() {
+        if ($(this).scrollTop() < 50) {
+            // hide background color of navbar
+            $("nav").removeClass("top-nav");
+            $("#back-to-top").fadeOut(); 
+        } else {
+            // show background color of navbar
+            $("nav").addClass("top-nav");
+            $("#back-to-top").fadeIn(); 
+        }
+    });
 });
 
 //smooth scrolling
 $(function() {
-	$("a.smooth-scroll").click(function(event) {
-		event.preventDefault();
-		// this will get/return the value of any anchor elem that is clicked
-		var section = $(this).attr("href");
+    $("a.smooth-scroll").click(function(event) {
+        event.preventDefault();
+        // this will get/return the value of any anchor elem that is clicked
+        var section = $(this).attr("href");
 
-		$("html, body").animate({
-			scrollTop: $(section).offset().top - 64
-		}, 1250, "easeInOutExpo");
-
-	});
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
