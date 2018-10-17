56adac920d6e2941b882d568a8fe5bb2537d5e27
Juan Ignacio Verni <JuanVerni@jiv-tb.lan>
2018-10-17 10:23:45 -0600

    media queries for small screens

--- js/custom.js
+++ js/custom.js
@@ -58,7 +58,25 @@ $(function() {
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsive: {
+            // breakpoint from 0 up
+            0   : {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480 : {
+                items: 3
+            },
+            // breakpoint from 768 up
+            768 : {
+                items: 5
+            },
+            // breakpoint from 992 up
+            992 : {
+                items: 6
+            }
+        }
     });
 });
 
