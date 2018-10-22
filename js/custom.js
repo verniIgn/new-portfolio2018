0cf02dfee13b03e6f752859733d34eed7f147b94
Juan Ignacio Verni <JuanVerni@JIV-TB.local>
2018-10-22 14:05:36 -0600

    js deleted for removed sections

--- js/custom.js
+++ js/custom.js
@@ -32,55 +32,6 @@ $(function() {
     });
 });
 
-// TESTIMONIALS SECTION
-$(function() {
-    $("#customers-testimonials").owlCarousel({
-        items: 1,
-        autoplay: true,
-        smartSpeed: 700,
-        loop: true,
-        autoplayHoverPause: true
-    });
-});
-
-// STATS COUNTER
-$(function() {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
-
-
-// CLIENTS SECTION
-$(function() {
-    $("#clients-list").owlCarousel({
-        items: 6,
-        autoplay: true,
-        smartSpeed: 700,
-        loop: true,
-        autoplayHoverPause: true,
-        responsive: {
-            // breakpoint from 0 up
-            0   : {
-                items: 1
-            },
-            // breakpoint from 480 up
-            480 : {
-                items: 3
-            },
-            // breakpoint from 768 up
-            768 : {
-                items: 5
-            },
-            // breakpoint from 992 up
-            992 : {
-                items: 6
-            }
-        }
-    });
-});
-
 // NAVBAR
 // this will hide or show navbar according to where we are in the site
 $(function() {
